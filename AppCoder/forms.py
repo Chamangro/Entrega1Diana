040bd6f1cde7f05ee3d44491926e2edd53eca718
Chamangro <federicodiana@outlook.com>
2022-04-19 20:43:45 -0300

    Second commit

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class LibroFormulario(forms.Form):
     titulo = forms.CharField()
@@ -14,3 +17,34 @@ class AutorFormulario(forms.Form):
 class GeneroFormulario(forms.Form):
     nombre = forms.CharField()
     descripcion = forms.CharField()
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # Saca los mensajes de ayuda
+        help_texts = {k:"" for k in fields}
+
+class UserEditForm(UserCreationForm):
+    
+    # Obligatorios
+    email = forms.EmailField(label="Ingrese su email:")
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Repetir la contraseña', widget=forms.PasswordInput)
+    
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2', 'last_name', 'first_name']
+
+
+class AvatarFormulario(forms.Form):
+    imagen = forms.ImageField()
+    
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Libro(models.Model):
     titulo=models.CharField(max_length=40)
     autor=models.CharField(max_length=40)
     genero=models.CharField(max_length=40)
     fecha=models.CharField(max_length=40)
-
+    
     def __str__(self):
         return f"Titulo: {self.titulo} - Autor: {self.autor}"
 
@@ -22,4 +23,11 @@ class Genero(models.Model):
     descripcion= models.CharField(max_length=180)
     
     def __str__(self):
-        return (f'Nombre del género: {self.nombre}')
+        return (f'Nombre del género: {self.nombre} /// Descripción {self.descripcion}')
+
+class Avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.imagen}"
